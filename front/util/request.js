3c66ceb2648eaf1c4d1b3ee85922c841295e46e9
wa-monster <2037485086@qq.com>
2020-01-16 17:32:49 +0800

    体积爱哦

---
+++ front/util/request.js
@@ -0,0 +1,30 @@
+
+
+
+export const req = function(options){
+	return new Promise((resolve,reject)=>{
+		options = Object.assign({},{url:'',method:''},options)
+		uni.request({
+			...options,
+			success(res){
+				resolve(res)
+			},
+			fail(err) {
+				reject(err)
+			}
+		})
+	})
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
